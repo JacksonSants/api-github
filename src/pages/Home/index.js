c4f380f5097cda767e90ff2c346ed48999e8005d
JacksonSants <jacksondiego2002@gmail.com>
2024-04-12 05:46:12 -0400

    Estilização de sessão de busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,21 @@
+import { Header } from '../../components/Header'
+import background from '../../assets/bg-github.png'
+import style from './style.css'
 function App() {
   return (
     <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img className="background" src={background} alt="Background App"/>
+
+        <div className="info">
+          <input name='user' placeholder='@username'></input>
+          <button>Buscar</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
